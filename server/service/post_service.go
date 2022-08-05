ff37754605bec15f38158f3fea20d8de4d9414a8
Neander Menezes <neander2801@gmail.com>
2022-08-05 13:33:25 -0300

    feat(server/post_service): declaring methods in interface, and implementing their skeleton for future coding.

--- server/service/post_service.go
+++ server/service/post_service.go
@@ -5,11 +5,17 @@ import (
 	pb "github.com/neandermenezes/gRPC-Upvote-Service/proto/pb"
 	"github.com/neandermenezes/gRPC-Upvote-Service/server/entity"
 	"github.com/neandermenezes/gRPC-Upvote-Service/server/repository"
+	"google.golang.org/protobuf/types/known/emptypb"
 	"log"
 )
 
 type PostService interface {
 	CreatePost(in *pb.Post, ctx context.Context) (*pb.PostId, error)
+	ReadPost(in *pb.PostId, ctx context.Context) (*pb.Post, error)
+	UpdatePost(in *pb.Post, ctx context.Context) (*emptypb.Empty, error)
+	DeletePost(in *pb.PostId, ctx context.Context) (*emptypb.Empty, error)
+	ListPosts(in *emptypb.Empty, stream pb.PostService_ListPostsServer) error
+	UpvotePost(in *pb.PostId, ctx context.Context) (*emptypb.Empty, error)
 }
 
 type service struct{}
@@ -38,4 +44,29 @@ func (*service) CreatePost(in *pb.Post, ctx context.Context) (*pb.PostId, error)
 	}
 
 	return res, err
+}
+
+func (s *service) ReadPost(in *pb.PostId, ctx context.Context) (*pb.Post, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *service) UpdatePost(in *pb.Post, ctx context.Context) (*emptypb.Empty, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *service) DeletePost(in *pb.PostId, ctx context.Context) (*emptypb.Empty, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *service) ListPosts(in *emptypb.Empty, stream pb.PostService_ListPostsServer) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *service) UpvotePost(in *pb.PostId, ctx context.Context) (*emptypb.Empty, error) {
+	//TODO implement me
+	panic("implement me")
 }
