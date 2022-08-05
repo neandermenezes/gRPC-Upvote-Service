2ee5be85f2f20dd8af6c56f41785647ab6799bee
Neander Menezes <neander2801@gmail.com>
2022-08-05 13:14:50 -0300

    feat(server/repository/post_repository): added post repository for handling the database.
    added CreatePost method: receives a PostItem and attempts to insert in MongoDB, returns postId and error

--- server/repository/post_repository.go
+++ server/repository/post_repository.go
@@ -1,4 +1,48 @@
 package repository
 
+import (
+	"context"
+	"fmt"
+	pb "github.com/neandermenezes/gRPC-Upvote-Service/proto/pb"
+	"github.com/neandermenezes/gRPC-Upvote-Service/server/entity"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type repo struct{}
+
+var (
+	Collection *mongo.Collection
+)
+
 type PostRepository interface {
+	CreatePost(in *entity.PostItem) (*pb.PostId, error)
+}
+
+func NewPostRepository() PostRepository {
+	return &repo{}
+}
+
+func (r *repo) CreatePost(data *entity.PostItem) (*pb.PostId, error) {
+	res, err := Collection.InsertOne(context.Background(), data)
+
+	if err != nil {
+		return nil, status.Errorf(
+			codes.Internal,
+			fmt.Sprintf("Internal error REPOSITORY: %v\n", err),
+		)
+	}
+
+	oid, ok := res.InsertedID.(primitive.ObjectID)
+
+	if !ok {
+		return nil, status.Errorf(
+			codes.Internal,
+			"Cannot convert to OID",
+		)
+	}
+
+	return &pb.PostId{Id: oid.Hex()}, nil
 }
