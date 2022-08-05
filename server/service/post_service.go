c20924f89d6dee78279c31ae30a8aab379ce5b25
Neander Menezes <neander2801@gmail.com>
2022-08-05 13:16:20 -0300

    feat(server/service/post_service): added post service for calling repository methods.
    added CreatePost method: receives a pb.Post, converts to a PostItem which is used to call repository.CreatePost. Returns a response and an error.

--- server/service/post_service.go
+++ server/service/post_service.go
@@ -1,1 +1,40 @@
 package service
+
+import (
+	pb "github.com/neandermenezes/gRPC-Upvote-Service/proto/pb"
+	"github.com/neandermenezes/gRPC-Upvote-Service/server/entity"
+	"github.com/neandermenezes/gRPC-Upvote-Service/server/repository"
+	"log"
+)
+
+type PostService interface {
+	CreatePost(in *pb.Post) (*pb.PostId, error)
+}
+
+type service struct{}
+
+var postRepository repository.PostRepository
+
+func NewPostService(repository repository.PostRepository) PostService {
+	postRepository = repository
+	return &service{}
+}
+
+func (*service) CreatePost(in *pb.Post) (*pb.PostId, error) {
+	log.Println("CreatePost service was invoked")
+
+	data := entity.PostItem{
+		AuthorName: in.AuthorName,
+		Title:      in.Title,
+		Content:    in.Content,
+		LikeCount:  0,
+	}
+
+	res, err := postRepository.CreatePost(&data)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return res, err
+}
