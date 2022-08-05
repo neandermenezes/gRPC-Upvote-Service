da20c820239ba1a3dbbdf4a71f0cfe356a9183fc
Neander Menezes <neander2801@gmail.com>
2022-08-05 13:11:28 -0300

    feat(server/main): added dependency injection and returned mongo connection to main func

--- server/main.go
+++ server/main.go
@@ -1,27 +1,49 @@
 package main
 
 import (
+	"context"
+	"github.com/joho/godotenv"
 	pb "github.com/neandermenezes/gRPC-Upvote-Service/proto/pb"
 	"github.com/neandermenezes/gRPC-Upvote-Service/server/controller"
+	"github.com/neandermenezes/gRPC-Upvote-Service/server/repository"
+	"github.com/neandermenezes/gRPC-Upvote-Service/server/service"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
 	"log"
 	"net"
+	"os"
+)
+
+var (
+	postRepository repository.PostRepository = repository.NewPostRepository()
+	postService    service.PostService       = service.NewPostService(postRepository)
+	postController controller.PostServer     = controller.NewPostController(postService)
 )
 
 func main() {
-	//ctx := context.Background()
-	//
-	//client, err := mongo.NewClient(options.Client().ApplyURI(uri))
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//
-	//err = client.Connect(ctx)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//
-	//collection = client.Database("upvote-db").Collection("posts")
+	ctx := context.Background()
+
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
+	uri := os.Getenv("MONGO_URI")
+	if uri == "" {
+		log.Fatal("No mongo uri set in env variables")
+	}
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	repository.Collection = client.Database("upvote-db").Collection("posts")
 	lis, err := net.Listen("tcp", "localhost:50051")
 	if err != nil {
 		log.Fatal("Failed to listen on 50051")
